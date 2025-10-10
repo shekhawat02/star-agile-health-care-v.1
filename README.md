67fbb1ade1a7a2fb5ff37985808fcf9bb5f93f1f
shekhawat02 <shekhawat02.g@gmail.com>
2025-10-10 20:29:12 +0530

    Initial commit for Health Care Project

---
+++ README.md
@@ -0,0 +1,1 @@
+# star-agile-health-care
---
+++ terraform-files/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "test-server" {
+  ami = "ami-0360c520857e3138f"
+  instance_type = "t2.micro"
+  key_name = "sir"
+  vpc_security_group_ids = ["sg-08348db6dca8c5940"]
+  connection {
+     type = "ssh"
+     user = "ubuntu"
+     private_key = file("./sir.pem")
+     host = self.public_ip
+     }
+  provisioner "remote-exec" {
+     inline = ["echo 'wait to start the instance' "]
+  }
+  tags = {
+     Name = "test-server"
+     }
+  provisioner "local-exec" {
+     command = "echo ${aws_instance.test-server.public_ip} > inventory"
+     }
+  provisioner "local-exec" {
+     command = "ansible-playbook /var/lib/jenkins/workspace/finance-project/terraform-files/ansibleplaybook.yml"
+     }
+  }
